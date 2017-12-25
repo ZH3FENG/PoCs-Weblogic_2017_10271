1943b8527361bd870b4ad199b2d289141c63b41c
ZH3FENG <mlanriver@gmail.com>
2017-12-25 21:23:31 +0800

    Update App.java

--- src/main/java/org/zh3feng/poc/App.java
+++ src/main/java/org/zh3feng/poc/App.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author: ZH3FENG
  * @Description:
- * @Date: 下午8:56 2017/12/25
+ * @Date: 下午8:56 2017/11/15
  * @Modified By:
  */
 @SpringBootApplication
